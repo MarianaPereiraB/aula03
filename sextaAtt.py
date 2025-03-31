2650d7d6e9ffeda93742e696aa7af37c0a7510d9
mariana pereira <marianapereirab2002@gmail.com>
2025-03-31 17:16:52 -0300

    jogo de futebol

--- sextaAtt.py
+++ sextaAtt.py
@@ -1,10 +1,14 @@
 time1 = input("insira o primeiro nome de time:")
 time2 = input("insira  o segundo nome de time:")
-gols1 = input("insira o número de gols do primeiro time:")
-gols2 = input(" insira o número de gols do segundo time:")
+gols1 = input(f"insira o número de gols do primeiro time: ")
+gols2 = input(f" insira o número de gols do segundo time: ")
+
 if gols1 > gols2:
     print(f"campeão é o {time1}")
 else:
-    print(f"campeão é o {time2}")
+    if gols2 > gols1:
+     print(f"campeão é o {time2}")
+    else:
+        print("empate")
 
 
