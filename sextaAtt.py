d6863e02d7588e55207c5f48ac444c4ee4e1c9cb
mariana pereira <marianapereirab2002@gmail.com>
2025-03-31 17:09:48 -0300

    sport campeão

---
+++ sextaAtt.py
@@ -0,0 +1,10 @@
+time1 = input("insira o primeiro nome de time:")
+time2 = input("insira  o segundo nome de time:")
+gols1 = input("insira o número de gols do primeiro time:")
+gols2 = input(" insira o número de gols do segundo time:")
+if gols1 > gols2:
+    print(f"campeão é o {time1}")
+else:
+    print(f"campeão é o {time2}")
+
+
