82b88804e660833ad269f0c97b64d9b587a92dad
mariana pereira <marianapereirab2002@gmail.com>
2025-03-31 14:53:53 -0300

    entrega da atividade da aula 3, salário e porcentagem

--- aula03.py
+++ aula03.py
@@ -1,5 +1,8 @@
 nome = input("nome")
 idade = int(input("idade"))
 salario =float(input("seu salário"))
-print(f"o nome é {nome}\n a idade é {idade}\n e o salário é {salario}")
+porcentagem = float(input("quantos % você quer aumentar seu salário?"))
+salarioatual = (porcentagem*salario)/100
+somaaumento = (salario+salarioatual)
+print(f"o nome é {nome}\n a idade é {idade}\n e o salário antes do aumento {salario}\n  porcentagem que vai ser aumentada no salário atual{porcentagem}% \n e o salário com o aumento{somaaumento}")
 #int 1 \ float 1.4
