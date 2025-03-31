3f323ce2f953381f0f113c1dfa8fea2b54932746
mariana pereira <marianapereirab2002@gmail.com>
2025-03-31 16:19:12 -0300

    atividade if e else

---
+++ terceiraAtividade.py
@@ -0,0 +1,9 @@
+from selectors import SelectSelector
+
+num1 = int(input("digite o primeiro número:"))
+num2 = int(input("digite o segundo número:"))
+
+if num1 < num2:
+    print(num2, num1)
+else:
+    print(num1,num2)
